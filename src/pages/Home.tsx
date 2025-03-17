80ef3dcf1f317908c7bd4a0ab359db6674309bb5
gabrielefasanoo <gabrielefasano13@gmail.com>
2025-03-17 21:25:47 +0100

    Aggiungi la pagina Home con componenti Navbar, Hero, About, Services, Team e Contact

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import Services from '../components/Services';
+import AboutSection from '../components/AboutSection';
+import TeamSection from '../components/TeamSection';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Hero />
+        <AboutSection />
+        <Services />
+        <TeamSection />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
